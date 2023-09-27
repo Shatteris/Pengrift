f150a65722069a5eebd564815f0395d59cf5a83e
lessSUNNYdays <114659667+lessSUNNYdays@users.noreply.github.com>
2023-09-27 22:55:58 +0100

    update movimento etc

--- Assets/SCRIPTS/PENGUIN.cs
+++ Assets/SCRIPTS/PENGUIN.cs
@@ -7,13 +7,19 @@ public class PENGUIN : MonoBehaviour
     public float jumpforce = 10f;
     public float jumpThreshold = 3f;
     public float jumpcooldown = 1f;
+    public float acceleration = 10;
+    public float lastzpos;
+    public LayerMask groundLayer;
+    public float maxX, minX;
 
     private bool canJump = true;
-    private bool isGrounded = true;
+    public bool isGrounded = true;
     private Rigidbody playerBody;
 
     void Start()
     {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        Input.gyro.enabled = true;
         if (!Input.gyro.enabled)
         {
             Debug.LogError("Gyroscope not enabled");
@@ -28,8 +34,8 @@ public class PENGUIN : MonoBehaviour
     {
         Vector3 Accel = Input.acceleration;
 
+        isGrounded = Physics.CheckSphere(transform.position - new Vector3(0, .5f, 0), .2f, groundLayer);
         Move();
-        isGrounded = Grounded();
         Jump();
     }
 
@@ -48,13 +54,15 @@ public class PENGUIN : MonoBehaviour
         {
             directionInput = 0f;
         }
+        Vector3 MoveInput = new Vector3(directionInput, 0, 0);
+        playerBody.AddForce(MoveInput * acceleration);
     }
 
     //Jump--------------------------------------------------------------------------------------
     private bool Grounded()
     {
         float rayLength = 0.01f;
-        Vector3 rayOrigin = transform.position + Vector3.up * 0.1f;
+        Vector3 rayOrigin = transform.position + Vector3.down * 0.1f;
 
         if (Physics.Raycast(rayOrigin, Vector3.down, rayLength))
         {
@@ -66,15 +74,18 @@ public class PENGUIN : MonoBehaviour
 
     private void Jump()
     {
-        float jumpInput = Input.acceleration.y;
+        float jumpInput = Input.acceleration.z;
+        Debug.Log("JumpInput: " + jumpInput);
 
-        if (jumpInput > 0.3f && isGrounded && canJump)
+        if (jumpInput > lastzpos && Mathf.Abs(jumpInput-lastzpos) >= 0.5f && isGrounded) //&& canJump)
         {
+            //canJump = true;
             playerBody.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+            Debug.Log("Jump");
             isGrounded = false;
-            canJump = false;
-            Invoke("EnableJump", jumpcooldown);
         }
+        Debug.Log("lastzpos: " + lastzpos);
+        lastzpos = Input.acceleration.z;
     }
 
     private void EnableJump()
@@ -82,5 +93,15 @@ public class PENGUIN : MonoBehaviour
         canJump = true;
     }
     //-------------------------------------------------------------------------------------------
+    private void OnDrawGizmosSelected()
+    {
+        // Set the gizmo color to red
+        Gizmos.color = Color.red;
 
+        // Calculate the position of the sphere cast
+        Vector3 sphereCenter = transform.position - new Vector3(0, .5f, 0);
+
+        // Draw the sphere cast gizmo
+        Gizmos.DrawWireSphere(sphereCenter, 0.2f);
+    }
 }
